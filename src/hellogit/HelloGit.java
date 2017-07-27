2373e990b507744d1720e6dcaefe92a7859b7e73
Administrator <Administrator@WANGXIANG2.Hisense.ad>
2017-07-27 10:37:17 +0800

    test the change of the project of meepo

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -3,5 +3,14 @@ package hellogit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git xxx");
+		System.out.println("真的很感谢站长推出的这个网站");
+		new hellogit.HelloGit().sayLove("meepo", 0.6);
+	}
+	
+	public void sayLove(String name, double d) {
+		if (d < 1)
+		{
+			System.out.println("我学的还行，找到了工作，爱你" + name);
+		}
 	}
 }
