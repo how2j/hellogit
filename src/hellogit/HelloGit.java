f4cebe564fd1d6e5f8317ea6f2d4f201aa7296de
Administrator <Administrator@WANGXIANG2.Hisense.ad>
2017-07-27 10:41:08 +0800

    change again

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -3,12 +3,12 @@ package hellogit;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git xxx");
-		System.out.println("真的很感谢站长推出的这个网站");
+		System.out.println("真的很感谢站长推出的这个网站，我是吴少");
 		new hellogit.HelloGit().sayLove("meepo", 0.6);
 	}
 	
-	public void sayLove(String name, double d) {
-		if (d < 1)
+	public void sayLove(String name, double year) {
+		if (year < 1)//year是找工作花的时间
 		{
 			System.out.println("我学的还行，找到了工作，爱你" + name);
 		}
