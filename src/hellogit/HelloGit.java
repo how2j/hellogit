9535588368b8222dafa27e378392565a71950afc
weiji1726 <34057089+weiji1726@users.noreply.github.com>
2017-11-29 11:58:52 +0800

    Update HelloGit.java

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,6 +2,6 @@ package hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("Only Trust Your Heart");
+		System.out.println("Only Trust Your Heart~~~");
 	}
 }
