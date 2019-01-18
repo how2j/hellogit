c5a186ef9cbfd4401a1186df0b2340affcc72c6a
yuhanzzz <35534741+yuhanzzz@users.noreply.github.com>
2019-01-18 15:02:59 +0800

    add newfile
    Change-Id: Icd933388e96b3d63ec3ff1750414dd32673e0af1

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,8 +2,7 @@ package hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-
-
-		System.out.println("Hello Git abc 随便改一改");
+		System.out.println("Hello Git");
+		System.out.println("The second line");
 	}
 }
