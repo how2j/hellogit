8058f373c3019ce80b4d3134af6b9c3646550195
初七不在家 <@192.168.232.1>
2019-02-15 10:26:08 +0800

    test

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -3,7 +3,8 @@ package hellogit;
 public class HelloGit {
 	public static void main(String[] args) {
 
-
 		System.out.println("Hello Git abc ");
+
+		System.out.println("这里是基于bobeng所做出的修改");
 	}
 }
