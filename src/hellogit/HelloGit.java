6d47363223faffa5bcbd830cd3b35ff01c5b811c
ZJC <704641708@qq.com>
2019-04-12 09:47:43 +0800

    分支fff

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -1,14 +1,17 @@
 package hellogit;
 
-public class HelloGit {
-	public static void main(String[] args) {
+import java.text.SimpleDateFormat;
 
-		System.out.println(666);
-		System.out.println("Hello Git abc ");
-		//1
-		//123
-		//分支
-		//ddd
-		//```
-	}
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println(666);
+        System.out.println("Hello Git abc ");
+        //1
+        //123
+        //分支
+        //ddd
+        //```
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+    }
 }
