4ac3020ee9ce9ce28c7f4a1e0addba4099a861ca
Shuna Li <shuna.li@ericsson.com>
2019-08-09 09:40:33 +0800

    test

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -1,9 +1,9 @@
 package hellogit;
 
 public class HelloGit {
-	public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
-		System.out.println("Hello Git abc ");
-	}
+        System.out.println("Hello Git abc lsn");
+    }
 }
