95d9d9f4b5639c0dd3a78e5b9b3b68be33b82e9c
jiangyi-git-account <66188268+jiangyi-git-account@users.noreply.github.com>
2020-05-31 10:31:10 +0800

    update HelloGit.java
    add  System.out.println("Hello Git abc 202005311029");

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,8 +2,7 @@ package hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-
-
 		System.out.println("Hello Git abc ");
+		System.out.println("Hello Git abc 202005311029");
 	}
 }
