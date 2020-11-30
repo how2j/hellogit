28ad0401a1761688312e58ab489cdc23a4cf9d3e
WHL <WHL@DESKTOP-Q8EGKNB>
2020-11-30 17:41:24 +0800

    增加了一点修改

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,8 +2,6 @@ package hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-
-
 		System.out.println("Hello Git abc ");
 		System.out.println("为HelloGit.java文件增加一点修改！");
 	}
