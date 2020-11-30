cbf60fbe8c02436cc78287cd92c79c52ff4e5b93
PA-WHL <65695247+PA-WHL@users.noreply.github.com>
2020-11-30 17:51:46 +0800

    Update HelloGit.java
    GitHub上直接增加了一点修改

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git abc ");
 		System.out.println("为HelloGit.java文件增加一点修改！");
+		System.out.println("GitHub上直接为HelloGit.java文件增加一点修改！");
 	}
 }
